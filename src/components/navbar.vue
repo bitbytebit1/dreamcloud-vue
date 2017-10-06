034cca18e29e576a07d9eb872623f99d6e485e0e
bitbytebit1 <tristan@tribe.nu>
2017-10-06 10:16:28 +0200

    Stripped navbar for mobile because I hate writing html.

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,21 +2,11 @@
   <div class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <a class="navbar-brand hidden-xs" href="#">DreamCloud</a>
         <a class="navbar-brand visible-xs" href="#">DC</a>
         <div class="navbar-form pull-left">
           <search></search>
         </div>
-      </div>
-      <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <li style="display:none"><a data-toggle="tab" id="search-results-tab" href="#search-results">Search</a></li>
-        </ul>
       </div>
     </div>
   </div>
