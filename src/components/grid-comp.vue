d39a3e1380509849d0d2ea4e613d8566cdc13dbc
bitbytebit1 <tristan@tribe.nu>
2017-10-11 00:30:06 +0200

    Netlify. Don't drink and code kids.

--- src/components/grid-comp.vue
+++ src/components/grid-comp.vue
@@ -81,7 +81,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style scoped>
 table {
   border: 2px solid #42b983;
   border-radius: 3px;
