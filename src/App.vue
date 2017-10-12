3f5bb203bc172f5266ac4be3967934f682e1f203
bitbytebit1 <tristan@tribe.nu>
2017-10-12 16:33:24 +0200

    Added Vuetify boiler plate

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <div id="app">
-    <navbar></navbar>
-    <div id="master" class='container-fluid'>
-      <main>
-        <router-view></router-view>
-      </main>  
-    </div>
-    <dcAudio :song="aSong"></dcAudio>
-  </div>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      clipped
+      persistent
+      v-model="drawer"
+      enable-resize-watcher
+      app
+      disable-route-watcher
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        DreamCloud
+      </v-toolbar-title>
+      <search></search>
+    </v-toolbar>
+
+    <main>
+      <v-content >
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+            <router-view></router-view>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </main>
+    <v-footer app fixed>
+    </v-footer>    
+  </v-app>
 </template>
 
 <script>
   import navbar from './components/navbar/navbar'
+  import search from './components/navbar/search'
   import dcAudio from './components/dcAudio'
   export default {
     name: 'app',
