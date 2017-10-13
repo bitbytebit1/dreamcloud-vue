42fc6ef34abcff64fce0db89e8100110784f322f
bitbytebit1 <tristan@tribe.nu>
2017-10-13 16:39:00 +0200

    Add current playlist component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <v-app id="inspire" dark>
     <v-navigation-drawer
+      v-model="drawerLeft"
       clipped
       persistent
-      v-model="drawer"
       enable-resize-watcher
       app
-      disable-route-watcher
-    >
+      disable-route-watcher>
       <v-list dense>
-        <v-list-tile @click="">
+        <v-list-tile :to="{path: '/home'}">
           <v-list-tile-action>
             <v-icon>dashboard</v-icon>
           </v-list-tile-action>
@@ -17,8 +16,7 @@
             <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-
-        <v-list-tile @click="">
+        <v-list-tile :to="{path: '/settings'}">
           <v-list-tile-action>
             <v-icon>settings</v-icon>
           </v-list-tile-action>
@@ -29,14 +27,25 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-toolbar app fixed clipped-left>
+    <v-toolbar app fixed clipped-left clipped-right>
       <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-side-icon @click.stop="drawerLeft = !drawerLeft"></v-toolbar-side-icon>
         DreamCloud
       </v-toolbar-title>
       <search></search>
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon @click.stop="drawerRight = !drawerRight"><v-icon large>playlist_play</v-icon></v-toolbar-side-icon>
     </v-toolbar>
-
+      <v-navigation-drawer
+      clipped
+      persistent
+      v-model="drawerRight"
+      enable-resize-watcher
+      app
+      disable-route-watcher
+      right
+      
+      ><current-playlist></current-playlist></v-navigation-drawer>
     <main>
       <v-content >
         <v-container fluid fill-height>
@@ -55,15 +64,18 @@
 <script>
   import search from './components/navbar/search'
   import dcAudio from './components/dcAudio'
+  import currentPlaylist from './components/playlist/current-playlist'
   export default {
     name: 'app',
     components: {
       'search': search,
-      'dcAudio': dcAudio
+      'dcAudio': dcAudio,
+      'current-playlist': currentPlaylist
     },
     data () {
       return {
-        drawer: false,
+        drawerLeft: false,
+        drawerRight: true,
         aSongs: [],
         aSong: []
       }
---
+++ src/components/playlist/current-playlist.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-container grid-list-sm>
+        <v-layout xs12 row wrap>
+            <v-flex xs12 flexbox @click="play" 
+            v-for="(song, index) in $store.getters.current_Playlist"
+            v-bind:song="song"
+            v-bind:index="index"
+            v-bind:key="index">
+            <v-card>
+                <v-card-media
+                v-lazy:background-image="song.posterLarge"
+                height="220px"
+                >
+                <v-container grid-list-xs fill-height fluid>
+                    <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text" v-text="song.title"></span>
+                        <div class="artist white--text" v-text="song.artist"></div>
+                    </v-flex>
+                    </v-layout>
+                </v-container>
+                </v-card-media>
+            </v-card>
+            </v-flex>
+
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'current-playlist',
+  data () {
+    return {
+      msg: 'Welcome to the real Trinity'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
