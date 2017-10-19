8fb434234c7ea8eaa4a7185882a0454fd9dfcc52
bitbytebit1 <tristan@tribe.nu>
2017-10-20 01:18:57 +0200

    blergh, affe finish him?

--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -16,22 +16,12 @@
         </v-layout>
         <scroll-to-top></scroll-to-top>
       </v-container>
-      <table v-if="toggle" class="table" style="width:100%;border:1px;">
-        <tr>
-          <th><p class="text-center">Title</p></th>
-          <th><p class="text-center">Bild</p></th>
-          <th><p class="text-center">Artist</p></th>
-          <th><p class="text-center">Uploaded</p></th>
-          <th><p class="text-center">Duration</p></th>
-        </tr>
-        <playlistItemList
-          v-for="(song, index) in songs"
-          v-bind:song="song"
-          v-bind:index="index"
-          v-bind:key="index"
-        >
-        </playlistItemList>
-      </table>
+        <v-container v-if="toggle" fluid>
+          <playlistItemList
+            v-bind:songs="songs"
+          >
+          </playlistItemList>
+        </v-container>
       <iframe :src="iframeSrc"></iframe>
   </v-flex>
 </template>
@@ -53,7 +43,7 @@ export default {
       showScrollToTop: false,
       msg: 'Welcome to the playlist Trinity',
       iframeSrc: '',
-      toggle: false
+      toggle: true
     }
   },
   computed: {
--- src/components/playlist/playlistItemList.vue
+++ src/components/playlist/playlistItemList.vue
@@ -1,49 +1,91 @@
 <template>
- <tr style="bgcolor:#eee">
-  <td v-on:click="play">
-    <div class="song-title" style="text-align:left; margin-left:30px; margin-top:10px;">
-      <p v-bind:class="{ active: isPlaying }">{{song.title}}</p>
-    </div>
-  </td>
-  <td v-on:click="play">
-    <img v-lazy="song.posterLarge" class="poster img-responsive" alt="" style="width: 30px; height: 30px;">
-  </td>
-  <td>
-    <a class="artist" :href="artistID">{{song.artist}}</a>
-  </td>
-  <td>
-    <span class="song-date">{{date}}</span>
-  </td>
-  <td>
-    <span class="song-duration">{{song.duration}}</span>
-  </td>
- </tr>
+  <v-data-table
+      v-bind:headers="headers"
+      :items="songs"
+      hide-actions
+      class="elevation-1"
+      :v-model="test"
+    >
+    <template slot="items" slot-scope="props" class="active">
+      <td @click="play(props.index)" :class="tdClass(props.item.mp32)"><img v-lazy="props.item.poster" height="35px" /></td>
+      <td @click="play(props.index)" :class="tdClass(props.item.mp32)">{{ props.item.title }}</td>
+      <td @click="play(props.index)" :class="tdClass(props.item.mp32)">{{ props.item.artist }}</td>
+      <td @click="play(props.index)" :class="tdClass(props.item.mp32)">{{ date(props.item.created) }}</td>
+      <td>
+      <v-menu
+        transition="slide-y-transition"
+        bottom
+        lazy
+      >
+      <v-btn icon slot="activator">
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+      <v-list>
+        <v-list-tile v-for="action in actions" :key="action.title" @click="action.func">
+          <v-list-tile-title>{{ action.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu></td>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 
 export default {
   name: 'playlistItemList',
-  props: ['song', 'index'],
+  props: ['songs'],
+  data () {
+    return {
+      actions: [{'title': 'Share', func: this.Share}],
+      test: [],
+      today: new Date(),
+      headers: [
+        {
+          text: 'Pic',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Title', value: 'title', align: 'left' },
+        { text: 'Artist', value: 'mp3', align: 'left' },
+        { text: 'Uploaded', value: 'created', align: 'left' },
+        { text: '', value: '', align: 'left', sortable: false }
+      ]
+    }
+  },
   computed: {
+    aSongsFilter: function () {
+      return this.$children[0].filteredItems
+    },
     artistID: function () {
       return '#/a/' + this.song.source + '/' + this.song.artist + '/' + this.song.artistID
     },
     img: function () {
       return this.song.posterLarge
-    },
-    date: function () {
-      return this.$DCAPI.calcDate(new Date(), this.song.created)
-    },
-    isPlaying: function () {
-      return this.$route.path === this.$store.getters.hash && this.index === this.$store.getters.index
     }
   },
   methods: {
-    play: function () {
-      this.$parent.play(this.index)
+    tdClass: function (link) {
+      return {
+        'active': this.isPlaying(link),
+        'text-xs-left': true
+      }
     },
-    share: function () {
+    isPlaying: function (link) {
+      if (this.$store.getters.index === -1) {
+        return
+      }
+      return this.$route.path === this.$store.getters.hash && link === this.$store.getters.current_Playlist[this.$store.getters.index].mp32
+    },
+    date: function (date) {
+      return this.$DCAPI.calcDate(this.today, date)
+    },
+    play: function (index) {
+      this.$store.commit('setNPlay', {songs: this.aSongsFilter, current: index, path: this.$route.path})
+      this.$DCPlayer.setNPlay(this.songs, index)
+    },
+    Share: function () {
       if (this.$UTILS.isMobile) {
         this.$parent.setIframeSrc('')
         this.$parent.setIframeSrc('whatsapp://send?text=' + encodeURI('http://dc42.netlify.com/#/t/' + this.song.source + '/' + this.song.title + '/' + this.song.title + '/' + this.song.trackID))
