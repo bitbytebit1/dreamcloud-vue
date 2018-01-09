fb74e8546858a69f0a0dc71b2fbedd5e8489fd3b
bitbytebit1 <tristan@tribe.nu>
2018-01-09 07:38:16 +0100

    Various youtube changes
    Added full screen button
    Experimented with closed captions

--- src/components/current-playlist/youtube-video.vue
+++ src/components/current-playlist/youtube-video.vue
@@ -1,9 +1,26 @@
 <template>
-  <v-flex xs12 lg-offset-2 lg9 flexbox class="ma-0 pa-0" >
-  <!-- <div> -->
-    <div :id="trackID" />
-    <!-- <iframe :id="trackID" width="auto" height="auto" :src="trackID" allowFullScreen frameborder="0"/> -->
-  <!-- </div> -->
+  <v-flex xs12 lg-offset-2 lg9 flexbox class="mt-3" >
+    <!-- Player -->
+    <v-flex xs12>
+      <div :id="this.current_trackID" />
+    </v-flex>
+
+    <!-- Title -->
+    <v-flex xs10>
+      <div class="title text-xs-left">{{$store.getters.current_song.title}}</div >
+    </v-flex> 
+
+    <!-- Buttons -->
+    <v-flex xs2>
+      <v-btn @click="fullscreenClick">
+        Fullscreen
+      </v-btn>
+    </v-flex>
+  
+    <!-- Description -->
+    <v-flex xs12>
+      <span class="subheading text-xs-left" style="white-space: pre;">{{description}}</span>    
+    </v-flex>
   </v-flex>
 </template>
 <script>
@@ -11,12 +28,19 @@
 /* eslint-disable */
 export default {
   name: 'youtube-video',
-  props: ['trackID'],
+  props: ['song'],
 
   data () {
     return {
       yt: '',
-      interval: ''
+      interval: '',
+      description: ''
+    }
+  },
+  filters: {
+    replaceNewline: function (value) {
+      value = value.replace(/\n/g, '<br>')
+      return value.replace(/\n/g, '<br>')
     }
   },
   computed: {
@@ -27,30 +51,64 @@ export default {
     }
   },
   mounted () {
+    this.getDesc()
     this.ytBind()
   },
   updated () {
-    // alert(1)
-    // this.ytBind()
-    this.yt.loadVideoById(this.trackID)
+    this.$store.getters.ytObject.loadVideoById(this.current_trackID)
+    this.getDesc()
   },
   methods: {
+    fullscreenClick () {
+      var e = document.getElementById(this.current_trackID);
+      if (e.requestFullscreen) {
+          e.requestFullscreen();
+      } else if (e.webkitRequestFullscreen) {
+          e.webkitRequestFullscreen();
+      } else if (e.mozRequestFullScreen) {
+          e.mozRequestFullScreen();
+      } else if (e.msRequestFullscreen) {
+          e.msRequestFullscreen();
+      }
+    },
+    getDesc () {
+      this.$DCAPI.getSongDescription(this.current_trackID, this.song.source, (resp) => {
+        
+        this.description = resp.items[0].snippet.description.trim()
+        console.log('"' + this.description + '"')
+      })
+    },
     ytBind () {
-      this.$DCPlayer.eAudio.pause()
       if (!this.$store.getters.ytVideo) {
         return
       }
+      this.$DCPlayer.eAudio.pause()
       this.yt = new YT.Player(this.current_trackID, {
         width: '100%',
-        // width: '300',
-        // height: '212',
-        videoId: this.trackID,
-        playerVars: { 'autoplay': 1, 'controls': 0 },
+        videoId: this.current_trackID,
+        enablejsapi: 1,
+        playerVars: {
+          autoplay: 1,        // Auto-play the video on load
+          controls: 0,        // Show pause/play buttons in player
+          color: 'teal',
+          showinfo: 0,        // Hide the video title
+          modestbranding: 1,  // Hide the Youtube Logo
+          fs: 0,              // Hide the full screen button
+          cc_load_policy: 1,  // Hide closed captions
+          iv_load_policy: 3,  // Hide the Video Annotations
+          rel: 0,             // Remove Related Videos
+          autohide: 1         // Hide video controls when playing
+        },
         events: {
           'onReady': this.ytReady,
-          'onStateChange': this.ytChanged
+          'onStateChange': this.ytChanged,
+          'onApiChange': this.ytOnApiChange
         }
       })
+    },
+    ytOnApiChange () {
+      // console.log(this.yt.getOptions())
+      // console.log(this.yt.getOptions('captions'))
     },
     ytReady (state) {
       this.$store.commit('ytObject', this.yt)
@@ -80,4 +138,14 @@ export default {
 </script>
 
 <style>
+/* .ytButtons{
+  float: right;
+}
+.ytTitle{
+  float: left;
+  margin-right:10px
+}
+.ytDesc{
+  float: left;
+} */
 </style>
