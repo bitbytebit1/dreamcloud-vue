2dd619cfb4e1a8bd6b42f4658a68797e937eaeb6
bitbytebit1 <tristan@tribe.nu>
2018-02-03 21:51:16 +0100

    Cleanup

---
+++ src/components/stage/stage.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-flex d-flex xs12 >
+    <video-stage></video-stage>
+    <audio-stage></audio-stage>
+    </v-layout>
+  </v-flex>
+</template>
+<script>
+/* eslint-disable */
+export default {
+  name: 'stage'
+}
+</script>
