4c18e7edfb9b955b542668423be2473960c28ed4
bitbytebit1 <tristan@tribe.nu>
2018-02-11 22:06:29 +0100

    updated lazy load

--- src/main.js
+++ src/main.js
@@ -23,8 +23,11 @@ import('../node_modules/vuetify/dist/vuetify.min.css')
 
 Vue.use(VueLazyload, {
   preLoad: 3,
-  loading: '/static/img/all.png',
-  attempt: 1
+  loading: '/static/img/loading2.gif',
+  error: '/static/img/loading2.gif',
+  attempt: 1,
+    // the default is ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
+  listenEvents: [ 'scroll' ]
 })
 Vue.use(UtilsPlug)
 // Vue.use(DCUserPlug)
