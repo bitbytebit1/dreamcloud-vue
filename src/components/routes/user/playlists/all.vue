bb21a8a89d042246f24fd09f687940432485b7d0
bitbytebit1 <tristan@tribe.nu>
2018-02-11 01:27:23 +0100

    playlists > all spinner fix

--- src/components/routes/user/playlists/all.vue
+++ src/components/routes/user/playlists/all.vue
@@ -1,24 +1,31 @@
 <template>
-  <v-flex v-if="aPlaylists" xs12 lg10 flexbox :key="$route.params.playlist">
-    <loading></loading>
-    <playlist rowsPerPage="84" :songs="allSongs"></playlist>
+  <v-flex xs12 lg10 flexbox :key="$route.params.playlist">
+    <loading v-if="!auth_state || !allSongs.length"></loading>
+    <playlist v-else :songs="allSongs" rowsPerPage="84"></playlist>
+
   </v-flex>
 </template>
 <script>
 import loading from '@/components/misc/loading'
-
+import { mapGetters } from 'vuex'
 export default {
   name: 'playlistsAll',
+  watch: {
+    'auth_state': 'bind'
+  },
+  created () {
+    this.bind()
+  },
   data () {
     return {
-      aPlaylists: 0,
-      loading: true
+      aPlaylists: []
     }
   },
   components: {
     'loading': loading
   },
   computed: {
+    ...mapGetters({auth_state: 'auth_state'}),
     allSongs () {
       let all = []
       for (const i1 in this.aPlaylists) {
@@ -28,13 +35,15 @@ export default {
           all.push(this.aPlaylists[i1].songs[i2])
         }
       }
-      this.loading = false
       return all
     }
   },
-  firebase () {
-    return {
-      aPlaylists: this.$DCFB.playlists
+  methods: {
+    bind () {
+      // only bind if logged in
+      if (this.auth_state) {
+        this.$bindAsArray('aPlaylists', this.$DCFB.playlists)
+      }
     }
   }
 }
