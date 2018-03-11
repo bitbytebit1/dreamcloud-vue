bf0b4c97219a2a2949652e8f9c43ce5ddc1ca7e6
bitbytebit1 <tristan@tribe.nu>
2018-03-11 22:21:40 +0100

    Added filter to sub overview

--- src/router/user/subs/overview.vue
+++ src/router/user/subs/overview.vue
@@ -1,12 +1,28 @@
 <template>
-  <v-container fluid v-bind="{ [`grid-list-sm`]: true }">
+  <v-container fluid grid-list-md justify-start>
+      <v-flex xs12 class="mr-4 ml-4 mb-3">
+      <!-- filter -->
+      <v-text-field
+        @focus="filterHasFocus = true"
+        @blur="filterHasFocus = false"
+        color="primary"
+        id="flr-txt"
+        label="Filter"
+        single-line
+        hide-details
+        v-model="search"
+        v-on:keyup.enter="$UTILS.closeSoftMobi()"
+        ref="search"
+      ></v-text-field>
+      </v-flex>
     <v-data-iterator
       v-if="$store.getters.auth_state"
       content-tag="v-layout"
       row
       wrap
-      class="mr-5 ml-5 mb-3"
+      class="mr-4 ml-4 mb-3"
       :items="subscriptions"
+      :search="search"
       :rows-per-page-items="rowsPerPageItems"
       pagination.sync="pagination"
       hide-actions
@@ -19,12 +35,13 @@
         sm6
         md4
         lg3
+        class="artist-card"
       >
         <!-- <v-card :to="{name: 'userPlaylist', params: {user: $route.params['user'], playlist: $route.params['playlist'], name: $route.params['name'] }}" class="pointer"> -->
-        <v-card :to="{name: 'artist', params: {source: props.item.source, artist: props.item.name, artistID: props.item.id }}" class="pointer">
+        <v-card :to="{name: 'artist', params: {source: props.item.source, artist: props.item.name, artistID: props.item.id }}" class="pointer" >
           <v-avatar
             class="mt-2"
-            size="75px"
+            size="95px"
           >
             <img
               :src="props.item.img"
@@ -45,27 +62,15 @@
 export default {
   name: 'userSubOverview',
   props: ['user'],
-  // components: {
-    // 'delete-button': deleteButton
-  // },
   data () {
     return {
-      // UID: this.$DCFB.UID,
+      filterHasFocus: false,
+      search: '',
       active: true,
       rowsPerPageItems: [{ text: "All", value: -1 }],
       pagination: {
         rowsPerPage: 'All'
       }
-    }
-  },
-  methods: {
-    x2 () {
-
-    }
-  },
-  computed: {
-    x3 () {
-      
     }
   },
   firebase () {
@@ -77,4 +82,7 @@ export default {
 </script>
 
 <style>
+.artist-card{
+  min-width: 303px;
+}
 </style>
