cb6da2fc596a2c32cbfb685e0a60a35e9f0b703a
bitbytebit1 <tristan@tribe.nu>
2018-03-28 10:43:58 +0200

    Custom scroll bar

--- src/App.vue
+++ src/App.vue
@@ -197,4 +197,28 @@
 .dchide{
   display: none !important;
 }
+
+/* width */
+#right-draw::-webkit-scrollbar {
+    width: 1px;
+}
+  
+::-webkit-scrollbar {
+    width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+    background: #555; 
+}
 </style>
