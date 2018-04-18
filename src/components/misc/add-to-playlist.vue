a923c380636954fccdf18ccd2612a6e8bc7f4427
bitbytebit1 <tristan@tribe.nu>
2018-04-18 15:56:27 +0200

    filter for add to playlist NEXTTICKTRASHFIX

--- src/components/misc/add-to-playlist.vue
+++ src/components/misc/add-to-playlist.vue
@@ -1,8 +1,8 @@
 <template>
-    <v-dialog :disabled="disabled" v-model="menuOpen" max-width="500px">
+    <v-dialog :disabled="disabled" v-model="menuOpen" max-width="500px" lazy>
     <v-btn :disabled="disabled" :color='btnCol' icon slot="activator" @click.stop="openMenu" >
       <v-icon>playlist_add</v-icon>
-    </v-btn>      
+    </v-btn>
       <v-list>
         <v-list-tile>
           <v-text-field
@@ -13,11 +13,28 @@
             single-line
             v-model="playlistName"
             v-on:keyup.enter="createNewPlaylist"
+            ref="filter"
           ></v-text-field>
         </v-list-tile>
-        <v-list-tile v-for="playlist in playlists" :key="playlist['.key']" @click="addToPlaylist(playlist)">
-          <v-list-tile-title>{{ playlist['name'] }}</v-list-tile-title>
-        </v-list-tile>
+        <v-data-iterator
+          v-if="$store.getters.auth_state"
+          :items="playlists"
+          :search="playlistName"
+          :rows-per-page-items="rowsPerPageItems"
+          hide-actions
+          no-data-text="Create new playlist"
+          no-results-text="Create new playlist"
+          :custom-filter="(items, search, filter) => { search = search.toString().toLowerCase() ; return items.filter(row => filter(row['name_lower'], search)) }"
+        >
+          <v-list-tile 
+            slot="item"
+            slot-scope="props"
+            :key="props.item['.key']" 
+            @click="addToPlaylist(props.item)"
+          >
+            <v-list-tile-title>{{ props.item['name'] }}</v-list-tile-title>
+          </v-list-tile>
+        </v-data-iterator>
       </v-list>
     </v-dialog>
 </template>
@@ -30,7 +47,14 @@ export default {
     return {
       btnCol: this.colour,
       playlistName: '',
-      menuOpen: false
+      menuOpen: false,
+      filterHasFocus: false,
+      search: '',
+      active: true,
+      rowsPerPageItems: [{ value: -1 }],
+      pagination: {
+        rowsPerPage: 'All'
+      }
     }
   },
   directives: {
@@ -50,9 +74,11 @@ export default {
     openMenu () {
       this.menuOpen = !this.menuOpen
       this.$emit('opened', true)
+      this.$nextTick(() => this.$refs.filter.focus())
     },
     btnFeedback () {
       //  this.menuOpen = this.$UTILS.isMobile ? false : true
+      this.playlistName = ''
       this.menuOpen = false
       this.btnCol = 'green'
       setTimeout(() => {
