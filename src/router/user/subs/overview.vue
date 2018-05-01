13d4e9f3db20c9d2b538adc15bd118e16308b245
bitbytebit1 <tristan@tribe.nu>
2018-05-01 13:49:36 +0200

    see last

--- src/router/user/subs/overview.vue
+++ src/router/user/subs/overview.vue
@@ -1,58 +1,59 @@
 <template>
-  <v-container fluid grid-list-md justify-start>
-      <v-flex xs12 class="mr-4 ml-4 mb-3">
-      <!-- filter -->
-      <v-text-field
-        @focus="filterHasFocus = true"
-        @blur="filterHasFocus = false"
-        color="primary"
-        id="flr-txt"
-        label="Filter"
-        single-line
-        hide-details
-        v-model="search"
-        v-on:keyup.enter="$UTILS.closeSoftMobi()"
-        ref="search"
-      ></v-text-field>
-      </v-flex>
-    <v-data-iterator
-      v-if="$store.getters.auth_state"
-      content-tag="v-layout"
-      row
-      wrap
-      class="mr-4 ml-4 mb-3"
-      :items="subscriptions"
-      :search="search"
-      :rows-per-page-items="rowsPerPageItems"
-      pagination.sync="pagination"
-      hide-actions
-    >
-      <v-flex
-        slot="item"
-        slot-scope="props"
-        xs12
-        sm6
-        md4
-        lg3
-        class="artist-card"
+  <v-flex flexbox xs12 lg10>
+    <v-container fluid grid-list-md>
+        <v-flex xs12 class="mr-4 ml-4 mb-3">
+        <!-- filter -->
+        <v-text-field
+          @focus="filterHasFocus = true"
+          @blur="filterHasFocus = false"
+          color="primary"
+          id="flr-txt"
+          label="Filter"
+          single-line
+          hide-details
+          v-model="search"
+          v-on:keyup.enter="$UTILS.closeSoftMobi()"
+          ref="search"
+        ></v-text-field>
+        </v-flex>
+      <v-data-iterator
+        v-if="$store.getters.auth_state"
+        content-tag="v-layout"
+        row
+        wrap
+        :items="subscriptions"
+        :search="search"
+        :rows-per-page-items="rowsPerPageItems"
+        pagination.sync="pagination"
+        hide-actions
       >
-        <!-- <v-card :to="{name: 'userPlaylist', params: {user: $route.params['user'], playlist: $route.params['playlist'], name: $route.params['name'] }}" class="pointer"> -->
-        <v-card :to="{name: 'artist', params: {source: props.item.source, artist: props.item.name, artistID: props.item.id }}" class="pointer" >
-          <v-avatar
-            class="mt-2"
-            :size="((!$store.getters.drawLeft ? 21 : 0) + 95 + (!$store.getters.drawRight ? 21 : 0)) + 'px'"
-          >
-            <img
-              :src="props.item.img"
-              alt=""
+        <v-flex
+          slot="item"
+          slot-scope="props"
+          xs12
+          sm6
+          md4
+          lg3
+          class="artist-card"
+        >
+          <!-- <v-card :to="{name: 'userPlaylist', params: {user: $route.params['user'], playlist: $route.params['playlist'], name: $route.params['name'] }}" class="pointer"> -->
+          <v-card :to="{name: 'artist', params: {source: props.item.source, artist: props.item.name, artistID: props.item.id }}" class="pointer" >
+            <v-avatar
+              class="mt-2"
+              :size="((!$store.getters.drawLeft ? 21 : 0) + 95 + (!$store.getters.drawRight ? 21 : 0)) + 'px'"
             >
-          </v-avatar>
-          <!-- <v-card-media :src="props.item.img" height="200px"></v-card-media> -->
-          <v-card-text class="text-xs-center">{{ props.item.name }}</v-card-text>
-        </v-card>
-      </v-flex>
-    </v-data-iterator>
-  </v-container>
+              <img
+                :src="props.item.img"
+                alt=""
+              >
+            </v-avatar>
+            <!-- <v-card-media :src="props.item.img" height="200px"></v-card-media> -->
+            <v-card-text class="text-xs-center">{{ props.item.name }}</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-data-iterator>
+    </v-container>
+  </v-flex>
 </template>
 <script>
 // /* eslint-disable */
@@ -81,6 +82,6 @@ export default {
 
 <style>
 .artist-card{
-  min-width: 303px;
+  /* min-width: 303px; */
 }
 </style>
