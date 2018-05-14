97c44ea7c4e17de4e2cb618eb56be24b87bed05d
bitbytebit1 <tribe321@gmail.com>
2018-05-14 02:51:41 +0200

    sidebar shuffle

--- src/components/sidebar-left/sidebar.vue
+++ src/components/sidebar-left/sidebar.vue
@@ -4,7 +4,60 @@
 
       <!-- Change to for loop to save the whales... I mean internet -->
 
+      <!-- home -->
+      <v-list-tile v-if="loggedIn" ripple @click="closeLeft" :to="{name: 'home'}">
+        <v-list-tile-action>
+          <v-icon>dashboard</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Home</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
 
+      <!-- Subscriptions all -->
+      <v-list-tile v-if="loggedIn" ripple @click="closeLeftOnMobile" :to="{name:'subsAll', params: {user: $DCFB.UID}}">
+        <v-list-tile-action>
+          <v-icon>music_note</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Trending</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+
+      <!-- Subscriptions Overview -->
+      <!-- <v-list-tile v-if="loggedIn" ripple @click="closeLeftOnMobile" :to="{name:'userSubOverview', params: {user: $DCFB.UID}}">
+        <v-list-tile-action>
+          <v-icon>view_module</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Subscriptions</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile> -->
+
+      <!-- Playlist Overview -->
+      <!-- <v-list-tile v-if="loggedIn" ripple @click="closeLeftOnMobile" :to="{name:'playlistOverview', params: {user: $DCFB.UID}}">
+        <v-list-tile-action>
+          <v-icon>view_module</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Playlists</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile> -->
+
+      <!-- history -->
+      <v-list-tile ripple class="history-link" @click="closeLeft" v-if="loggedIn" :to="{name:'history', params: {user: $DCFB.UID}}">
+        <v-list-tile-action>
+          <v-icon>history</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>History</v-list-tile-title>
+        </v-list-tile-content>
+        <span class="delete">
+          <delete-button @delete="clearHistory"></delete-button>
+        </span>
+      </v-list-tile>
+
+      <!-- about -->
       <v-list-tile v-if="!loggedIn" ripple @click="closeLeft" :to="{name: 'about'}">
         <v-list-tile-action>
           <v-icon>info</v-icon>
@@ -14,6 +67,7 @@
         </v-list-tile-content>
       </v-list-tile>
 
+      <!-- login -->
       <v-list-tile v-if="!loggedIn" ripple @click="closeLeft" :to="{path: '/login'}">
         <v-list-tile-action>
           <v-icon>person</v-icon>
@@ -22,56 +76,29 @@
           <v-list-tile-title>Login</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
-
-
-      <v-list-tile ripple v-if="loggedIn" @click="closeLeft" :to="{name: 'home'}">
+      
+      <!-- settings -->
+      <v-list-tile ripple @click="closeLeft" v-if="loggedIn" :to="{path: '/settings'}">
         <v-list-tile-action>
-          <v-icon>dashboard</v-icon>
+          <v-icon>settings</v-icon>
         </v-list-tile-action>
         <v-list-tile-content>
-          <v-list-tile-title>Home</v-list-tile-title>
+          <v-list-tile-title>Settings</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
-
-      <v-list-group v-if="loggedIn" :value="userDrawer" prepend-icon="person" no-action>
-        <v-list-tile ripple slot="activator">
-          <v-list-tile-content>
-            <v-list-tile-title>User</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        <v-list-tile ripple class="history-link" @click="closeLeft" v-if="loggedIn" :to="{name:'history', params: {user: $DCFB.UID}}">
-          <v-list-tile-action>
-            <v-icon>history</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>History</v-list-tile-title>
-          </v-list-tile-content>
-          <span class="delete">
-            <delete-button @delete="clearHistory"></delete-button>
-          </span>
-        </v-list-tile>
-      
-
-        <v-list-tile ripple @click="closeLeft" v-if="loggedIn" :to="{path: '/settings'}">
-          <v-list-tile-action>
-            <v-icon>settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Settings</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list-group>
     </v-list>
     
+    <!-- Playists -->
     <v-list dense class="pa-0">
       <user-playlists @closeLeft="closeLeft" v-if="loggedIn"></user-playlists>
     </v-list>
     
+    <!-- Subscriptions -->
     <v-list dense class="pa-0">
       <user-subscriptions @closeLeft="closeLeft" v-if="loggedIn"></user-subscriptions>
     </v-list>
 
+    <!-- tos -->
     <v-list dense class="pa-0 mb-5">
       <v-list-tile ripple @click="closeLeft" v-if="!loggedIn" :to="{path: '/tos'}">
         <v-list-tile-action>
@@ -105,7 +132,6 @@ export default {
       this.$emit('closeLeft', 'left')
     },
     clearHistory () {
-      // alert('bai')
       this.$DCFB.historyClear()
     }
   },
