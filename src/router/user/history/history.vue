43b2f0002da00fc6a850efaf03ac2741ae029bc7
bitbytebit1 <tribe321@gmail.com>
2018-05-16 04:26:04 +0200

    History fix
    Shows most recently played always

--- src/router/user/history/history.vue
+++ src/router/user/history/history.vue
@@ -1,7 +1,7 @@
 <template>
   <v-flex xs12 lg10>
     <loading v-if="!auth_state || !aHistory.length"></loading>
-    <playlist v-else rowsPerPage='84' :songs="aHistory"></playlist>
+    <playlist v-else rowsPerPage='84' :songs="aHistRev"></playlist>
   </v-flex>
 </template>
 <script>
@@ -25,12 +25,15 @@ export default {
   methods: {
     bind () {
       if (this.auth_state) {
-        this.$bindAsArray('aHistory', this.$DCFB.history, null, () => { this.aHistory = this.$UTILS.uniqueArray(this.aHistory.reverse()) })
+        this.$bindAsArray('aHistory', this.$DCFB.history)
       }
     }
   },
   computed: {
-    ...mapGetters({auth_state: 'auth_state'})
+    ...mapGetters({auth_state: 'auth_state'}),
+    aHistRev () {
+      return this.$UTILS.uniqueArray([...this.aHistory].reverse())
+    }
   },
   created () {
     this.bind()
