d500f6c2c2ed8d51ed6dd37635308bb1111816ed
bitbytebit <tribe321@gmail.com>
2019-06-23 18:14:13 +0200

    added auto fix rules for linter

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,25 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const path = require('path');
 
 module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .options({
+        fix: true,
+      });
+  },
   configureWebpack: {
     plugins: [
       new VuetifyLoaderPlugin()
-    ]
+    ],
+    resolve: {
+      extensions: ['.js', '.vue', '.json', '.scss'],
+      alias: {
+        styles: path.resolve(__dirname, 'src/assets/scss'),
+      }
+    }
   },
   // chainWebpack: config => {
   //   config.module
@@ -17,7 +32,7 @@ module.exports = {
   //       return options;
   //     });
   // },
-  lintOnSave: undefined,
+  lintOnSave: true,
   devServer: {
     overlay: {
       warnings: true,
@@ -29,56 +44,56 @@ module.exports = {
     workboxOptions: {
       importScripts: ['update-available.js'],
       runtimeCaching: [{
-          urlPattern: /^https:\/\/fonts\.googleapis\.com\/|^https:\/\/fonts\.gstatic\.com\//,
-          handler: 'cacheFirst',
-          options: {
-            cacheName: 'font-cache',
-            expiration: {
-              maxEntries: 100
-            },
-            cacheableResponse: {
-              statuses: [
-                0,
-                200
-              ]
-            }
-          }
-        },
-        // http:\/\/84\.157\.227\.137:7000\/api
-        {
-          urlPattern: /^https:\/\/dc-mp3-wwlveeistv\.now\.sh|https:\/\/cors\.io\/\?https:\/\/www\.saveoffline\.com\/process|^https:\/\/www\.s\d{1,2}\.saveoffline\.com|^https:\/\/www\.saveoffline\.com\/get\/\?i|^https:\/\/www\.s\d{1,2}\.saveoffline\.com|^https:\/\/www\.saveoffline\.com\/get\/\?i|^https:\/\/api\.soundcloud\.com\/tracks\/.+\/stream|^https:\/\/sndcdn\.com/,
-          handler: 'cacheFirst',
-          options: {
-            cacheName: 'song-cache',
-            expiration: {
-              maxAgeSeconds: 1210000
-            },
-            cacheableResponse: {
-              statuses: [
-                0,
-                200,
-                302
-              ]
-            }
-          }
-        },
-        // Match search API links, store as cache first for 3 hours
-        {
-          urlPattern: new RegExp(/^https:\/\/www\.googleapis\.com\/youtube\/v3|https:\/\/api\.mixcloud|^https:\/\/api\.soundcloud\.com\/users|^https:\/\/api\.soundcloud\.com\/tracks|^https:\/\/api.vimeo.com?/),
-          handler: 'cacheFirst',
-          options: {
-            cacheName: 'search-cache',
-            expiration: {
-              maxAgeSeconds: 10800
-            },
-            cacheableResponse: {
-              statuses: [
-                0,
-                200
-              ]
-            }
+        urlPattern: /^https:\/\/fonts\.googleapis\.com\/|^https:\/\/fonts\.gstatic\.com\//,
+        handler: 'cacheFirst',
+        options: {
+          cacheName: 'font-cache',
+          expiration: {
+            maxEntries: 100
+          },
+          cacheableResponse: {
+            statuses: [
+              0,
+              200
+            ]
           }
         }
+      },
+      // http:\/\/84\.157\.227\.137:7000\/api
+      {
+        urlPattern: /^https:\/\/dc-mp3-wwlveeistv\.now\.sh|https:\/\/cors\.io\/\?https:\/\/www\.saveoffline\.com\/process|^https:\/\/www\.s\d{1,2}\.saveoffline\.com|^https:\/\/www\.saveoffline\.com\/get\/\?i|^https:\/\/www\.s\d{1,2}\.saveoffline\.com|^https:\/\/www\.saveoffline\.com\/get\/\?i|^https:\/\/api\.soundcloud\.com\/tracks\/.+\/stream|^https:\/\/sndcdn\.com/,
+        handler: 'cacheFirst',
+        options: {
+          cacheName: 'song-cache',
+          expiration: {
+            maxAgeSeconds: 1210000
+          },
+          cacheableResponse: {
+            statuses: [
+              0,
+              200,
+              302
+            ]
+          }
+        }
+      },
+      // Match search API links, store as cache first for 3 hours
+      {
+        urlPattern: new RegExp(/^https:\/\/www\.googleapis\.com\/youtube\/v3|https:\/\/api\.mixcloud|^https:\/\/api\.soundcloud\.com\/users|^https:\/\/api\.soundcloud\.com\/tracks|^https:\/\/api.vimeo.com?/),
+        handler: 'cacheFirst',
+        options: {
+          cacheName: 'search-cache',
+          expiration: {
+            maxAgeSeconds: 10800
+          },
+          cacheableResponse: {
+            statuses: [
+              0,
+              200
+            ]
+          }
+        }
+      }
       ]
     },
     name: 'dreamcloud'
