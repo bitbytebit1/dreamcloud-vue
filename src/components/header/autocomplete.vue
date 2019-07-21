4be9b780dc4e03a26fb21578a86a2518f55c8e3a
bitbytebit <tribe321@gmail.com>
2019-07-21 07:17:53 +0200

    autocomplete white whale?
    4nders fix finally

--- src/components/header/autocomplete.vue
+++ src/components/header/autocomplete.vue
@@ -15,6 +15,7 @@
     solo
     clearable
     @keyup.enter='enter'
+    @blur="select = search"
   >
     <template 
       slot="item" 
@@ -48,18 +49,23 @@ export default {
     },
     enter () {
       // closes auto complete menu
-      this.$refs.auto.$children[0].isActive = false
+      this.$refs.auto.$children[0].isContentActive = false
       this.emit(this.search)
     },
     emit (v) {
       this.$emit('search', v)
     },
     querySelections (v) {
+      if (this.$refs.auto.$children[0].isContentActive == false) {
+        // console.log('resetting isContentActive due to input change')
+        this.$refs.auto.$children[0].isContentActive = true
+      }
       this.loading = true
+      this.items = [v]
       this.$jsonp(`https://suggestqueries.google.com/complete/search?callback=?&hl=en&ds=yt&jsonp=suggestCallBack&client=youtube&q=${v}`, {callbackName: 'suggestCallBack'})
         .then(json => {
-          this.items = json[1].map(v => v[0])
-          this.items.unshift(v)
+          this.items = this.items.concat(json[1].map(v => v[0]))
+          // this.items.unshift(v)
           this.loading = false
         }).catch(() => {
           this.loading = false
